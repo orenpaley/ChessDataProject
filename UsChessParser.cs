b3741bd476393a152c0493fea777ffccfa77d839
Oren <53711317+orenpaley@users.noreply.github.com>
2025-03-21 19:28:40 -0500

    added reg and quick rating get methods to usChessParser class

--- UsChessParser.cs
+++ UsChessParser.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.Metrics;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -21,9 +22,33 @@ class UsChessParser
         HtmlDocument htmlDoc = web.Load(memberHomePage + memberId);
         return htmlDoc;
     }
-    public HtmlNodeCollection getValues(string memberId)
+    public HtmlNodeCollection GetValues(string memberId)
 	{
         var doc = GetMemberHomePage(memberId);
         return doc.DocumentNode.SelectNodes("//b");
 	}
+    public static string GetRegularRating(HtmlNodeCollection values)
+    {
+        string regularRatingRaw = values[2].ChildNodes[0].InnerHtml;
+        Regex ratingRegex = new Regex("([0-9]{3,4})");
+        string regularRatingBeforeRegex = "";
+
+        foreach (char c in regularRatingRaw)
+        {
+            if (c.Equals('&'))
+            {
+                break;
+            }
+            regularRatingBeforeRegex += c;
+        }
+        Match mRating = ratingRegex.Match(regularRatingBeforeRegex);
+        return mRating.Value;
+    }
+
+    public static string GetRegularRatingDateUpdated(string rawRegularRating)
+    {
+        Regex dateRegex = new Regex("([0-9]{4}-[0-9]{2})");
+        Match mDate = dateRegex.Match(rawRegularRating);
+        return mDate.Value;
+    }
 }
